7fdf6fca45af3be1cd9c2828994bc4dc8cd08a76
kpaje <kelvinmembership@gmail.com>
2019-08-02 22:38:44 -0700

    created props and destructured

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-const Pet = () => {
+const Pet = ({ name, animal, breed }) => {
 	return React.createElement("div", {}, [
-		React.createElement("h1", {}, "Oscar"),
-		React.createElement("h2", {}, "Dog"),
-		React.createElement("h2", {}, "Yorkie")
+		React.createElement("h1", {}, name),
+		React.createElement("h2", {}, animal),
+		React.createElement("h2", {}, breed)
 	]);
 };
 
 const App = () => {
 	return React.createElement("div", {}, [
 		React.createElement("h1", {}, "Adopt Me!"),
-		React.createElement(Pet),
-		React.createElement(Pet),
-		React.createElement(Pet)
+		React.createElement(Pet, {
+			name: "Oscar",
+			animal: "Dog",
+			breed: "Maltese"
+		}),
+		React.createElement(Pet, {
+			name: "Bellamie",
+			animal: "Dog",
+			breed: "Yorkie"
+		}),
+		React.createElement(Pet, {
+			name: "Lucky",
+			animal: "Dog",
+			breed: "Shepard"
+		})
 	]);
 };
 
