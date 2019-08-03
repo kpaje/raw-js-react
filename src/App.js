d545b54c08decf9bddbcc4ded5fd3e45658ec0b9
kpaje <kelvinmembership@gmail.com>
2019-08-02 22:30:20 -0700

    created Pet component and passed into App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+const Pet = () => {
+	return React.createElement("div", {}, [
+		React.createElement("h1", {}, "Oscar"),
+		React.createElement("h2", {}, "Dog"),
+		React.createElement("h2", {}, "Yorkie")
+	]);
+};
+
 const App = () => {
-	return React.createElement(
-		"div",
-		{},
-		React.createElement("h1", {}, "Adospt Me!")
-	);
+	return React.createElement("div", {}, [
+		React.createElement("h1", {}, "Adopt Me!"),
+		React.createElement(Pet),
+		React.createElement(Pet),
+		React.createElement(Pet)
+	]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
