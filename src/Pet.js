41153fdcf1a87dae654baabc524419e1f9b40ee2
kpaje <kelvinmembership@gmail.com>
2019-08-03 01:46:32 -0700

    raw react and JSX comparison

--- src/Pet.js
+++ src/Pet.js
@@ -1,8 +1,21 @@
 import React from "react";
-export const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+
+function Pet({ name, animal, breed }) {
+  //   return React.createElement("div", {}, [
+  //     React.createElement("h1", {}, name),
+  //     React.createElement("h2", {}, animal),
+  //     React.createElement("h2", {}, breed)
+  //   ]);
+
+  //top and bottom are equivalent
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+  );
+}
+
+export default Pet;
